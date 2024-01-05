718565aedde0a67af0ce0c7bd42c63cfaecf5df8
HimanshuGupta673 <himanshuu673@gmail.com>
2024-01-05 19:42:12 +0530

    Done with first page

--- screens/Walkthrough.js
+++ screens/Walkthrough.js
@@ -2,7 +2,7 @@ import { View, Text, Image } from 'react-native'
 import React from 'react'
 import PageContainer from '../components/PageContainer'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import { images, COLORS, SIZES, FONTS } from '../constants'
+import { images, SIZES, FONTS } from '../constants'
 import Button from '../components/Button'
 
 export default function Walkthrough({ navigation }) {
